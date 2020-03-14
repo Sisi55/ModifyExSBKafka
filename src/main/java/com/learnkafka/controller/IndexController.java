b87a2864d914626bd6ad4fd07bbaacdea02e14b2
Sisi55 <dqgsh1055@gmail.com>
2020-03-15 03:11:02 +0900

    add feature/auth

--- src/main/java/com/learnkafka/controller/IndexController.java
+++ src/main/java/com/learnkafka/controller/IndexController.java
@@ -1,19 +1,29 @@
 package com.learnkafka.controller;
 
+import com.learnkafka.config.auth.LoginUser;
+import com.learnkafka.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+//    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         /*
 userName
 clickCount
 shopCount
 */
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 }
